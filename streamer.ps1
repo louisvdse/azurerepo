dc9011c5ce903edc51856a574c8b75b156405f21
louisvdse <60323391+louisvdse@users.noreply.github.com>
2020-01-26 18:48:30 +0100

    Add files via upload

---
+++ streamer.ps1
@@ -0,0 +1,4 @@
+$customConfig = @{
+  "fileUris" = (,"https://raw.githubusercontent.com/louisvdse/azurerepo/master/streamer.exe");
+  "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File streamer.ps1"
+}
